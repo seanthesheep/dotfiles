7fb75d30a12f1256663a0f567fbf1ba8ddf2c564
Sean Sullivan <seansullivan@Seans-MacBook-Pro.local>
2025-07-01 13:37:41 -0400

    update dotfile

--- setup.sh
+++ setup.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
+
+# ~Manually install NeoVim
+mkdir -p ~/.local
+cd ~/.local
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod u+x nvim-linux-x86_64.appimage
+./nvim-linux-x86_64.appimage --appimage-extract
+mkdir ~/.local/bin
+ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
+
+echo "XDG_CONFIG_HOME=$HOME" >>~/.profile
+cd ~/.config/nvigit clone https://github.com/LazyVim/starter ~/.config/nvimm
+
+cd /workspaces/$RepositoryName
+
 echo '[[ -v gh_pat ]] && export GITHUB_TOKEN=$gh_pat' >>~/.bashrc
